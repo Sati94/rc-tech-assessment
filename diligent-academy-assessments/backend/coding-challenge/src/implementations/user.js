4fcf52e282996e7e599a7dea28c590d6a0c75f64
Ati <nagyonkellvalamihez@gmail.com>
2024-08-21 10:12:55 +0200

    add function to the user.js

--- diligent-academy-assessments/backend/coding-challenge/src/implementations/user.js
+++ diligent-academy-assessments/backend/coding-challenge/src/implementations/user.js
@@ -4,18 +4,52 @@ export class User {
     }
 
     borrow(book, library) {
-        throw new Error('Not implemented');
+        try {
+            this.books.push(book);     //add book to books 
+            library.removeBook(book);  // remove book the library
+            return book;
+
+        } catch (e) {
+            throw new Error('Can not add book to the user booklist!', e);
+        }
+
     }
 
     hasBook(book) {
-        throw new Error('Not implemented');
+        try {
+
+            this.books.filter(name => name === book.name); // searching the book in the books Array
+            return true;
+
+        } catch (e) {
+            throw new Error('This book is not borrowed !', e);
+        }
     }
+
 
     return(book, library) {
-        throw new Error('Not implemented');
+        try {
+            const bookIndex = this.books.indexOf(book);  // Searching the bookIndex
+            if (bookIndex !== -1) {
+                this.books.splice(bookIndex, 1);         // remove the book
+                library.addBook(book);
+            }
+            return false;
+
+        } catch {
+            return true;
+        }
     }
 
+
     getBookNames() {
-        throw new Error('Not implemented');
+        try {
+
+            return this.books.map(book => book.name)  // Searching the book.name
+
+        } catch (e) {
+            throw new Error('This book not avaiable !', e);
+        }
+
     }
 }
